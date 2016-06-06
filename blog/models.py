1559818d079154366ab657a7be485c994195cc72
insoo <inu0616@gmail.com>
2016-06-06 14:54:48 +0900

    blog앱,accounts앱 생성 및 모델 정의

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    view = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    message = models.TextField()
+    commenter = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
